41ecb7fe0c34258f195305fa17351882412f2306
Subho4156 <subhayanp.cse.jisu22@gmail.com>
2025-06-06 00:50:01 +0530

    Some Changes for Deployment

--- server/index.js
+++ server/index.js
@@ -26,10 +26,10 @@ app.use('/api/auth', authRoutes)
 app.use('/api/passwords', passwordRoutes)
 
 if(process.env.NODE_ENV=== "production"){
-    app.use(express.static(path.join(__dirname, "/client/dist")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.get(/(.*)/ ,(req,res)=> {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
     })
 }
 
